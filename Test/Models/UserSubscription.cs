707968b391955f2ab1d410032cb7d9de17eb6844
jossimu017 <jossimu017@B215-04>
2020-09-03 13:07:19 +0200

    Add project files.

---
+++ Test/Models/UserSubscription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.Models
+{
+    public class UserSubscription
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Jmeno { get; set; }
+        [Required]
+        public string Prijmeni { get; set; }
+        
+    }
+}
